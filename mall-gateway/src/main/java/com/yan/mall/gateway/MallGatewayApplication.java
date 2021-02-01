0050e31e616536a91a3846bac1fa0a049365a946
huyan <huyan@111.com.cn>
2021-02-01 17:52:09 +0800

    网关层mall-gateway提交

---
+++ mall-gateway/src/main/java/com/yan/mall/gateway/MallGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.yan.mall.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@Slf4j
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallGatewayApplication.class, args);
+        log.info("网关启动成功...");
+    }
+
+}
